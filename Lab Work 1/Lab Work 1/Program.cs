14e8274a1a8b71f173ab67b4613af35b2fe5de8b
Solariantos <72076645+Solariantos@users.noreply.github.com>
2020-09-29 14:50:50 +0300

    Add files via upload

---
+++ Lab Work 1/Lab Work 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Lab_Work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.Write("Input the high value of conе - ");
+            int highValue = int.Parse(Console.ReadLine());
+            Console.Write("Input the radius value of conе - ");
+            int radiusValue = int.Parse(Console.ReadLine());
+            double capacity = (Math.PI * highValue * Math.Pow(radiusValue,2));
+            Console.WriteLine("Capacity of cone equals - " + capacity);
+
+        }
+    }
+}
---
+++ Lab work 3/Lab work 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Construct_if_else_if
+{
+    class Program
+    {
+        /*   7.	Перевірити істинність вислову:
+          "Серед трьох даних цілих чисел є хоч би одна пара співпадаючих".  */
+        static void Main()
+        {
+            int[] arr = new int[3];
+            string temp = "Are there a pair of integers that are the same? - ";
+      
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("Enter the "+(j+1)+" integer number : ");
+                arr[j] = int.Parse(Console.ReadLine());
+            }
+            if (arr[0] == arr[1] || arr[0] == arr[2] || arr[1] == arr[2])
+            {
+                Console.WriteLine(temp + true);
+            }
+            else
+            {
+                Console.WriteLine(temp + false);
+            }
+        }
+    }
+}
