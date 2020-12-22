90c650754c5c719b69f361ce59540d354aaf2161
Solariantos <72076645+Solariantos@users.noreply.github.com>
2020-12-23 00:06:27 +0200

    Laboratorna 11
    Laboratorna 11

---
+++ Laboratorna 11/lab_11/STO.cs
@@ -0,0 +1,13 @@
+﻿namespace lab_11
+{
+    class STO
+    {
+        public void collapseClimbDone(Car car) => car.collapseClimbDone = true;
+        public void painted(Car car) => car.painted = true;
+        public void oilChangePerformed(Car car) => car.oilChangePerformed = true;
+        public void fullTechnicalInspectionPassed(Car car) => car.fullTechnicalInspectionPassed = true;
+        public void wheelReplacementPerformed(Car car) => car.wheelReplacementPerformed = true;
+        public void bodyRepairPerformed(Car car) => car.bodyRepairPerformed = true;
+    
+    }
+}
