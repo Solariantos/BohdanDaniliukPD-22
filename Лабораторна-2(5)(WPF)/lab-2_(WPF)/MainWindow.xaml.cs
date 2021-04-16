e7f41e78850481206d343ec39bbfe36783d5b66d
Solariantos <72076645+Solariantos@users.noreply.github.com>
2021-04-16 21:52:25 +0300

    Add files via upload

---
+++ Лабораторна-2(5)(WPF)/lab-2_(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace lab_2__WPF_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ButtonsField bf = new ButtonsField(textBox2);
+            bf.drawField(gridForButtons);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.Text != "")
+            {
+                comboBox.Items.Add(textBox.Text);
+                textBox.Text = "";
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (comboBox.Items.Count != 0)
+                comboBox.Items.RemoveAt(comboBox.Items.Count - 1);
+            else
+                MessageBox.Show("И что мне удалять?", "Комбо Бокс пустой");
+        }
+
+        private void comboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
